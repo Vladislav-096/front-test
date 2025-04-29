3bf318d1e70faf8041d1d918629952874682bf9d
VladislavVanyushkin <vlad.vanyushkin.96@mail.ru>
2025-04-29 16:17:50 +0300

    feat: changed dishStatistic function that it could work with proxy server

--- src/app/dishStatistic.ts
+++ src/app/dishStatistic.ts
@@ -1,34 +1,53 @@
 import { validateResponse } from "./validationResponse";
 
-const API_URL = "https://api.together.xyz/v1/chat/completions";
-
 interface GetDisgStatistic {
   dishName: string;
   signal?: AbortSignal;
 }
 
+const API_URL = import.meta.env.VITE_BACKEND_PROXY_URL;
+
 export const getDishStatistic = async ({
   dishName,
   signal,
 }: GetDisgStatistic) => {
-  const prompt = `How many calories, proteins, fats, and carbohydrates are in 100 grams of ${dishName}? Return the answer as a JSON string with fields for calories, proteins, fats, and carbohydrates, and a separate field with a comment about the glycemic index in English. Do not include ANYTHING else in the response (no extra symbols or words) except the JSON string. But if ${dishName} is not a food, then return an empty string as an answer (no need any JSON string anymore)`;
-  return fetch(API_URL, {
+  return fetch(`${API_URL}/dish-statistics`, {
     method: "POST",
     signal,
-    headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      // model: "thudm/glm-4-32b:free",
-      model: "meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8",
-      messages: [{ role: "user", content: prompt }],
-      temperature: 0.3,
-    }),
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ dishName }),
   })
     .then(validateResponse)
     .catch((err) => {
-      console.log("getDisgStatistic function error", err);
+      console.log("getDishStatistic function error", err);
       throw err;
     });
 };
+
+// const API_URL = "https://api.together.xyz/v1/chat/completions";
+
+// export const getDishStatistic = async ({
+//   dishName,
+//   signal,
+// }: GetDisgStatistic) => {
+//   const prompt = `How many calories, proteins, fats, and carbohydrates are in 100 grams of ${dishName}? Return the answer as a JSON string with fields for calories, proteins, fats, and carbohydrates, and a separate field with a comment about the glycemic index in English. Do not include ANYTHING else in the response (no extra symbols or words) except the JSON string. But if ${dishName} is not a food, then return an empty string as an answer (no need any JSON string anymore)`;
+//   return fetch(API_URL, {
+//     method: "POST",
+//     signal,
+//     headers: {
+//       Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       // model: "thudm/glm-4-32b:free",
+//       model: "meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8",
+//       messages: [{ role: "user", content: prompt }],
+//       temperature: 0.3,
+//     }),
+//   })
+//     .then(validateResponse)
+//     .catch((err) => {
+//       console.log("getDisgStatistic function error", err);
+//       throw err;
+//     });
+// };
